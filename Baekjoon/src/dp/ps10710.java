0a9269825f7d977333c76d4dd35348edcd0edbc8
Jeon Jihyeon <jihyean2004@naver.com>
2023-07-11 23:12:27 +0900

    feat. new package

--- Baekjoon/src/dp/ps10710.java
+++ Baekjoon/src/dp/ps10710.java
@@ -1,4 +1,4 @@
-package slidingWindow;
+package dp;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,58 +9,45 @@ import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class ps10710 {
-
+	//이전 값을 저장하면서, 이전값과 now 값을 비교하여 min값을 갱신한다 -> dp (nap)
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		ArrayList<Integer> cities = new ArrayList<>();
-		ArrayList<Integer> diffs = new ArrayList<>();
+		int[] cities = new int[N+1];
+		int[] diffs = new int[M+1];
+		int[][] CD = new int[N+1][M+1];
 		
-		for(int i=0; i<N; i++) {
+		for(int i=1; i<=N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
-			cities.add(a);
+			cities[i] = a;
 		}
 		
-		for(int i=0; i<M; i++) {
+		for(int i=1; i<=M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
-			diffs.add(a);
+			diffs[i] = a;
 		}
 		
-		Collections.sort(cities, new Comparator<Integer>() {
-
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				return o1-o2;
+		for(int i=1; i<=N; i++) {
+			for(int j=i; j<=i+M-N; j++) {
+				CD[i][j] = cities[i] * diffs [j] ;
+				
+				if(j==i) { //i와 j가 같은 즉, 각 행의 첫 열은 무조건 min. 전 행과 min 값 비교하면 안 됨.
+					CD[i][j] += CD[i-1][j-1];
+					continue;
+				}
+				//해당행의 전열 과 <-> 전행의 전열을 비교해서 min 값으로 설정.
+				CD[i][j] = Math.min(CD[i][j-1], CD[i][j]+CD[i-1][j-1]);
 			}
-		});
-		
-		Collections.sort(diffs, new Comparator<Integer>() {
 			
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				return o1-o2;
-			}
-		});
-		
-		ArrayList<Integer> sortedDiffs = new ArrayList<>();
-		
-		for(int i=N-1; i>=0; i--) {
-			sortedDiffs.add(diffs.get(i));
 		}
 		
-		int sum = 0;
-		for(int i=0; i<N; i++) {
-			sum += cities.get(i)*sortedDiffs.get(i);
-		}
-		
-		System.out.println(sum);
+		System.out.println(CD[N][M]);
 		
 	}
 
